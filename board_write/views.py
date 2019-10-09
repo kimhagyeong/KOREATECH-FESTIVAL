da6fb6d1c555ead3c05a1d91c84a12f28c2b83be
Do-ho <hong7511059@koreatech.ac.kr>
2019-10-09 11:57:17 +0900

    Refactoring

--- board_write/views.py
+++ board_write/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Board_write
+from .models import Board
 # Create your views here.
 
 def new(request) :
