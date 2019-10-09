c1d9ab8d0704f960fad7b16310c8ad8118db087d
이재영 <dldustn14@gmail.com>
2019-10-09 11:40:30 +0900

    admin 등록

--- board_write/models.py
+++ board_write/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Board_write(models.Model) :
     title = models.CharField(max_length=200)
-    date = models.DateTimeCheckMixin(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     image = models.ImageField(upload_to='images/', blank=True)
