e92af306bfb59a64e6893644db63ff63c91e235a
Do-ho <hong7511059@koreatech.ac.kr>
2019-10-09 13:03:34 +0900

    navigating

--- board_detail/models.py
+++ board_detail/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    post = models.IntegerField()                            #댓글 PK
+    author = models.CharField(max_length=15)                #작성자
+    text = models.TextField()                               #댓글 내용
+    created_date = models.DateTimeField(auto_now_add=True)  #작성 일자
+    approved_comment = models.BooleanField(default=False)   #승인 여부
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- koreatechfestival/urls.py
+++ koreatechfestival/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 import main.views
 import board_write.views
 import board_main.views
+import board_detail.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',main.views.home, name = 'home'),
     path('board/new/',board_write.views.new, name='new'),
     path('board/',board_main.views.board, name='board'),
+    path('board/<int:board_id>', board_detail.views.detail, name='detail'),
 ]
