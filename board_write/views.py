11ac74f4b1e5978190466b85743804479a62d0dd
이재영 <dldustn14@gmail.com>
2019-10-09 11:52:01 +0900

    board_new url

--- board_write/views.py
+++ board_write/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Board_write
 # Create your views here.
+
+def new(request) :
+    return render(request,'new.html')
