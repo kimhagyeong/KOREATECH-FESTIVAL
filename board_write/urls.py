4a8d5637de4ef08fefbdfea1952fbe8e06cc0337
이재영 <dldustn14@gmail.com>
2019-10-09 16:08:05 +0900

    url path 정의

--- board_write/urls.py
+++ board_write/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-import main.views
-import board_write.views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',main.views.home, name = 'home'),
-    #path('create/', views.createPost, name= 'createPost'),
-    
+    #path('',main.views.home, name = 'home'),
+    path('', views.new, name='new'),
+    path('create/', views.createPost, name='createPost'),
 ]
