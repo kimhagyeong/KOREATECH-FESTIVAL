4427073083293cc2d263dd656e94e02f4aefb5d9
UiHyeon <yh4268@koreatech.ac.kr>
2019-10-09 12:51:35 +0900

    board_main first commit

--- koreatechfestival/urls.py
+++ koreatechfestival/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 import main.views
 import board_write.views
+import board_main.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',main.views.home, name = 'home'),
-    path('board/new/',board_write.views.new, name='new')
+    path('board/new/',board_write.views.new, name='new'),
+    path('board/',board_main.views.board, name='board'),
 ]
