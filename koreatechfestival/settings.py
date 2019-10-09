7675e3514bb3cd4e82c6f3baee73b74e4a0cd4d4
이재영 <dldustn14@gmail.com>
2019-10-09 11:28:20 +0900

    앰 정의

--- koreatechfestival/settings.py
+++ koreatechfestival/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'board_write',
+    'board_main',
+    'board_detail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
